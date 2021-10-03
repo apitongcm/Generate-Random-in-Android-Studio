8185fc8086dadfe040364d22d0ee1849b24dc33a
apitongcm <apitongcm@gmail.com>
2021-10-03 10:17:21 +0800

    Onclick change random image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -18,9 +19,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val arr = arrayOf("Hello", "How are you?", "Welcome, Handsome", "Play game","Mobile Legends na this!!")
-        val index = (0..4).random()
-        resultText.text = arr[index]
+
+        val randomize = (1..6).random()
+        val drawableResource = when(randomize)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
